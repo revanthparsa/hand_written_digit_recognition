bc311e831c805d708e7ed90c2bb2fd4f4d089496
revanthparsa <revanth.06parsa@gmail.com>
2021-09-22 01:16:46 +0530

    Creating functions

--- hand_written_digit_recognition/plot_digits_1.py
+++ hand_written_digit_recognition/plot_digits_1.py
@@ -10,7 +10,9 @@ import os
 from joblib import load, dump
 
 digits = datasets.load_digits()
-rescale_factors = [4,8,16]
+rescale_factors = [8]
+os.mkdir('models')
+lst_train_valid = [(0.15, 0.15)]
 
 def preprocess(images, rescale_factor):
     image_resized = []
@@ -28,81 +30,45 @@ def create_splits(images, targets, test_size, valid_size):
 def test(X_valid, y_valid):
     predicted_valid = clf.predict(X_valid)
     acc_valid = accuracy_score(predicted_valid, y_valid)
-    f1_valid = fl_score(predicted_valid, y_valid , average="macro")
+    f1_valid = f1_score(predicted_valid, y_valid , average="macro")
     return {'acc':acc_valid,'f1':f1_valid}
-# flatten the images
-n_samples = len(digits.images)
-data_1 = digits.images
-print("Size of the image: {}".format(data_1.shape))
-print()
-data_1 = data_1.reshape((n_samples, -1))
 
-for test_size, valid_size in [test_size,valid_size]:
+model_candidates = []
+for test_size, valid_size in lst_train_valid:
     for rescale_factor in rescale_factors:
+        print('-'*50)
         for gamma_idx in [10 ** exp for exp in range(-6, 4)]:
             image_resized = preprocess(digits.images, rescale_factor)
-            image_resized = image_resized.reshape((n_samples, -1))
-            clf1 = svm.SVC(gamma=gamma_idx)
+            image_resized = np.array(image_resized)
+            image_resized = image_resized.reshape((len(digits.images), -1))
+            clf = svm.SVC(gamma=gamma_idx)
             X_train, X_valid, X_test, y_train, y_valid, y_test = create_splits(
-                images, targets, test_size, valid_size)
-            
+                image_resized, digits.target, test_size, valid_size)
+            clf.fit(X_train, y_train)
+            metric_dic = test(X_valid, y_valid)
+            if metric_dic['acc'] < 0.11:
+                print("Skipping for gamma {}".format(gamma_idx))
+                continue
+            candidate = {
+                "acc_valid" : metric_dic['acc'],
+                "f1_valid" : metric_dic['f1'],
+                "gamma" : gamma_idx,
+                "rescale_factor": rescale_factor
+            }
+            model_candidates.append(candidate)
+            output_folder = "./models/test_{}_val_{}_rescale_{}_gamma_{}".format(
+                test_size, valid_size, rescale_factor, gamma_idx)
+            os.mkdir(output_folder) 
+            dump(clf, os.path.join(output_folder,"model.joblib"))
+            print('gamma: {}, {}x{} ==> {} ==> {}'.format(gamma_idx,
+                rescale_factor, rescale_factor, metric_dic['acc'], metric_dic['f1']))
 
-os.mkdir('models')
-#For finding the best gamma value
 
-
-total_len = len(X_train) + len(X_valid) + len(X_test)
-print(f'Percentage of samples in Train dataset: {round((len(X_train)/total_len)*100,2)},\
-        Valid dataset: {round((len(X_valid)/total_len)*100,2)},\
-        Test dataset: {round((len(X_test)/total_len)*100,2)}')
-print('-'*50)
-gamma_list = []
-accuracy_gamma = []
-model_candidates = []
-for index in range(10):
-    gamma_idx = 10**(-6+index)
-    gamma_list.append(gamma_idx)
-    clf = svm.SVC(gamma = gamma_idx)
-    clf.fit(X_train, y_train)
-
-    accuracy_gamma.append(acc_valid)
-    if acc_valid < 0.11:
-        print("Skipping for gamma {}".format(gamma_idx))
-        continue
-    candidate = {
-        "acc_valid" : acc_valid,
-        "gamma" : gamma_idx
-    }
-    model_candidates.append(candidate)
-    output_folder = "./models/test_{}_val_{}_rescale_{}_gamma_{}".format(
-        round((len(X_test)/total_len)*100,2), round((len(X_valid)/total_len)*100,2),
-        8, gamma_idx)
-    os.mkdir(output_folder) 
-    dump(clf, os.path.join(output_folder,"model.joblib"))
-
-'''
-table = [['S.No','Gamma', 'Validation Accuracy']]
-for i in range(len(gamma_list)):
-  t = []
-  t.append(i+1)
-  t.append(gamma_list[i])
-  t.append(accuracy_gamma[i])
-  table.append(t)
-print(tabulate(table, headers='firstrow'))
-
-max_accuracy = max(accuracy_gamma)
-max_acc_index = accuracy_gamma.index(max_accuracy)
-clf = svm.SVC(gamma = gamma_list[max_acc_index])
-clf.fit(X_train, y_train)
-'''
-max_acc_valid = max(model_candidates,key = lambda x:x["acc_valid"])
+max_acc_valid = max(model_candidates, key = lambda x:x["acc_valid"])
 best_model_folder =  "./models/test_{}_val_{}_rescale_{}_gamma_{}".format(
-        round((len(X_test)/total_len)*100,2), round((len(X_valid)/total_len)*100,2),
-        8, max_acc_valid['gamma'])
+    lst_train_valid[0][0], lst_train_valid[0][1], max_acc_valid['rescale_factor'], max_acc_valid['gamma'])
 clf = load(os.path.join(best_model_folder,"model.joblib"))
-predicted_test = clf.predict(X_test)
-test_accuracy = (accuracy_score(predicted_test, y_test))
-
+metric_dic = test(X_valid, y_valid)
 print('-'*50)
-print('Best gamma is {}, Test accuracy on best gamma is {}'.format(max_acc_valid['gamma'], test_accuracy))
+print('Best gamma is {}, Test accuracy {}, F1-Score is {}'.format(max_acc_valid['gamma'], metric_dic['acc'], metric_dic['f1']))
 print('-'*50)
